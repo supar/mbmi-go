37c8cccee60f7999ad72b5463d361d0631d57937
supar <paulrez@gmail.com>
2018-01-11 15:16:30 +0300

    Rewrite midllewares

--- middleware.go
+++ middleware.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"net/http"
+	"net/http/httputil"
 	"strings"
 )
 
@@ -23,40 +24,19 @@ func JWT(log LogIface) WrapHandler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			var (
-				//claims TokenClaims
-				ctx  context.Context
-				data []byte
-				err  error
-				id   string
-				res  ResponseIface
-				t    string
-				tk   *Token
+				ctx context.Context
+				err error
+
+				id = r.Context().Value("Id").(string)
+				tk = NewToken([]byte("secret"))
 			)
 
-			id = r.Context().Value("Id").(string)
-			tk = NewToken([]byte("secret"))
-
-			if t = r.Header.Get("Authorization"); t != "" {
+			if t := r.Header.Get("Authorization"); t != "" {
 				tk.JWT = strings.TrimPrefix(t, "Bearer ")
 
 				if err = tk.Parse(); err != nil {
 					log.Error("%s: Can't parse token: %s", id, err.Error())
 				}
-			}
-
-			if r.URL.Path != "/login" {
-				if !tk.Valid() {
-					log.Error("%s: Token is nil or not valid", id)
-
-					res = NewResponseError(401, nil)
-					http.Error(w, "", res.Status())
-					data, _ = res.Get()
-					w.Write(data)
-
-					return
-				}
-
-				log.Debug("%s: Token is valid", id)
 			}
 
 			ctx = context.WithValue(r.Context(), "Token", tk)
@@ -67,50 +47,32 @@ func JWT(log LogIface) WrapHandler {
 	}
 }
 
-// Send static files to the client
-func Assets(dir string) WrapHandler {
-	return func(next http.Handler) http.Handler {
-		var (
-			h http.Handler
-		)
-
-		if dir != "" {
-			h = http.FileServer(http.Dir(dir))
-		}
-
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			var (
-				t string
-			)
-
-			if t = r.Header.Get("Accept"); t == "application/json" {
-				next.ServeHTTP(w, r)
-				return
-			}
-
-			if h != nil {
-				h.ServeHTTP(w, r)
-				return
-			}
-
-			http.Error(w, http.StatusText(406), 406)
-		})
-	}
-}
-
 //  Set request id
-func RequestId(log LogIface) WrapHandler {
+func RequestId() WrapHandler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			var (
 				id  = RandStringId(12)
-				ctx = context.WithValue(context.Background(), "Id", id)
+				ctx = context.WithValue(r.Context(), "Id", id)
 			)
 
 			r = r.WithContext(ctx)
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+// Print request data
+func verbose(log LogIface) WrapHandler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			var (
+				id      = r.Context().Value("Id").(string)
+				dump, _ = httputil.DumpRequest(r, true)
+			)
 
 			log.Notice("%s: %s, %s: %s %s", id, r.RemoteAddr, r.UserAgent(), r.Method, r.URL.RequestURI())
-			log.Debug("%s: #%v", id, r.Header)
+			log.Debug("%s: %s", id, dump)
 
 			next.ServeHTTP(w, r)
 
