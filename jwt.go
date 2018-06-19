a815a08b8c5a1dc9387df20cb3ab57bb5ff68b29
supar <paulrez@gmail.com>
2018-06-19 10:20:43 +0300

    Add to interface more functions
    Returns Issuer, Subject
    Add functions comments

--- jwt.go
+++ jwt.go
@@ -6,7 +6,7 @@ import (
 	"mbmi-go/models"
 )
 
-// JSON web token object
+// Token represent user authorization data
 type Token struct {
 	JWT      string `json:"jwt"`
 	secret   []byte
@@ -14,27 +14,39 @@ type Token struct {
 	t        *jwt.Token
 }
 
+// IdentityIface is used to work with Token data
 type IdentityIface interface {
 	Identity() int64
+	Issuer() string
+	Valid() bool
+	Subject() string
 }
 
+// TokenClaims represents extention for the standard claims
+// from JWT package
 type TokenClaims struct {
-	Uid int64 `json:"uid"`
+	uid int64 `json:"uid"`
 	jwt.StandardClaims
 }
 
-func NewClaims() TokenClaims {
+// NewClaims returns new token claims
+func NewClaims(uid int64, subject string) TokenClaims {
 	return TokenClaims{
-		StandardClaims: jwt.StandardClaims{},
+		uid: uid,
+		StandardClaims: jwt.StandardClaims{
+			Subject: subject,
+		},
 	}
 }
 
+// NewToken returns new token
 func NewToken(secret []byte) (t *Token) {
 	return &Token{
 		secret: secret,
 	}
 }
 
+// Sign token
 func (s *Token) Sign(claims TokenClaims) *Token {
 	s.t = jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	s.JWT, _ = s.t.SignedString(s.secret)
@@ -42,6 +54,7 @@ func (s *Token) Sign(claims TokenClaims) *Token {
 	return s
 }
 
+// Parse JWT string with TokenClaims and validate token
 func (s *Token) Parse() (err error) {
 	s.t, err = jwt.ParseWithClaims(s.JWT, &TokenClaims{}, func(t *jwt.Token) (interface{}, error) {
 		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
@@ -54,18 +67,33 @@ func (s *Token) Parse() (err error) {
 	return
 }
 
+// Identity returns user id
 func (s *Token) Identity() int64 {
 	if s.t != nil {
-		return s.t.Claims.(*TokenClaims).Uid
+		return s.t.Claims.(*TokenClaims).uid
 	}
 
 	return -1
 }
 
+// Valid returns token state: true if token valid
 func (s *Token) Valid() bool {
 	if s.t != nil {
 		return s.t.Valid
 	}
 
 	return false
+}
+
+// Issuer returns issue value
+func (s *Token) Issuer() string {
+	return s.t.Claims.(*TokenClaims).Issuer
+}
+
+// Subject returns token subject
+func (s *Token) Subject() string {
+	if s.t != nil {
+		return s.t.Claims.(*TokenClaims).Subject
+	}
+	return ""
 }
